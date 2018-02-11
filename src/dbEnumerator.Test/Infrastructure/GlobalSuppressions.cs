a47c55b41ae384d7030d524c850c6a6de4414afb
Sergio Navarro Pino <sergio.navarro@analyticalways.com>
2018-02-11 18:16:38 +0100

    Supression file for naming warnings

---
+++ src/dbEnumerator.Test/Infrastructure/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:dbEnumerator.Test.dbEnumeratorShould")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:dbEnumerator.Test.EnumHelperShould.detect_a_enum_not_based_on_int")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:dbEnumerator.Test.EnumHelperShould.ensure_that_a_type_is_enum")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:dbEnumerator.Test.EnumHelperShould.get_enum_description")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:dbEnumerator.Test.EnumHelperShould.get_empty_description_if_here_si_no_description")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:dbEnumerator.Test.EnumHelperShould.detect_a_type_that_is_not_an_enum")]
+
